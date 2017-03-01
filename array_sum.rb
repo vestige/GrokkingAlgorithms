b391ad83b06780354542da71ed73cebc1f312760
vestige <vestige_sync@me.com>
2017-03-01 18:17:33 +0900

    modify car cdr

--- array_sum.rb
+++ array_sum.rb
@@ -1,9 +1,7 @@
 def sum_list(list)
-  if (list.size == 0) 
-    return 0
-  else
-    return list[0] + sum_list(list.drop(1))
-  end
+  return 0 if list.empty?
+  car, *cdr = list
+  car + sum_list(cdr)
 end
 
 a = [1, 2, 3, 4, 5, 6, 7, 8]
