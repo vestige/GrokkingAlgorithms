8c2334df3a0a7f8c490428ebbe44386b0f690703
vestige <vestige_sync@me.com>
2017-03-02 22:47:25 +0900

    add method max

--- array_sum.rb
+++ array_sum.rb
@@ -1,8 +1,25 @@
-def sum_list(list)
+def sum(list)
   return 0 if list.empty?
   car, *cdr = list
-  car + sum_list(cdr)
+  car + sum(cdr)
+end
+
+def count(list)
+  return 0 if list.empty?
+  car, *cdr = list
+  1 + count(cdr)
+end
+
+def max(list)
+  car, *cdr = list
+  if (cdr.size == 1)
+    return (car > cdr[0]) ? car : cdr[0]
+  end
+  tmp_max = max(cdr)
+  (car > tmp_max) ? car : tmp_max
 end
 
 a = [1, 2, 3, 4, 5, 6, 7, 8]
-p sum_list(a)
+p sum(a)
+p count(a)
+p max(a)
