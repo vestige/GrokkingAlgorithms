0f7c442179cc6b37b031bf7037bad05b28fb20cc
vestige <vestige_sync@me.com>
2017-03-03 22:17:13 +0900

    modify args car cdr

--- array_sum.rb
+++ array_sum.rb
@@ -1,25 +1,20 @@
-def sum(list)
-  return 0 if list.empty?
-  car, *cdr = list
-  car + sum(cdr)
+def sum(car, *cdr)
+  return 0 if cdr.empty?
+  car + sum(*cdr)
 end
 
-def count(list)
-  return 0 if list.empty?
-  car, *cdr = list
-  1 + count(cdr)
+def count(car, *cdr)
+  return 1 if cdr.empty?
+  1 + count(*cdr)
 end
 
-def max(list)
-  car, *cdr = list
-  if (cdr.size == 1)
-    return (car > cdr[0]) ? car : cdr[0]
-  end
-  tmp_max = max(cdr)
+def max(car, *cdr)
+  return (car > cdr[0]) ? car : cdr[0] if (cdr.size == 1)
+  tmp_max = max(*cdr)
   (car > tmp_max) ? car : tmp_max
 end
 
-a = [1, 2, 3, 4, 5, 6, 7, 8]
-p sum(a)
-p count(a)
-p max(a)
+a = [10, 1, 20, 3, 20, 4, 5, 6, 7, 8]
+p sum(*a)
+p count(*a)
+p max(*a)
